4142c11fe5b69b78de072f9778f6af9b790f62c8
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-11-20 22:53:18 -0300

    feat: add controller project

---
+++ src/main/java/br/dev/dantas/point/controller/EndPointController.java
@@ -0,0 +1,15 @@
+package br.dev.dantas.point.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("teste")
+public class EndPointController {
+
+    @GetMapping("hi")
+    public String hi() {
+        return "OMAE WA MOU SHIDEIRU";
+    }
+}
