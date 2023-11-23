f6fb537ec8f7aa60029a332b6faccfbd21a59c19
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-11-22 21:52:16 -0300

    feat: component scan e controllers

--- src/main/java/br/dev/dantas/point/PointApplication.java
+++ src/main/java/br/dev/dantas/point/PointApplication.java
@@ -2,12 +2,19 @@ package br.dev.dantas.point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"test.outside", "br.dev.dantas.point"})
 public class PointApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PointApplication.class, args);
+
+		var applicationContext = SpringApplication.run(PointApplication.class, args);
+		Arrays.stream(applicationContext.getBeanDefinitionNames())
+				.forEach(System.out::println);
 	}
 
 }
