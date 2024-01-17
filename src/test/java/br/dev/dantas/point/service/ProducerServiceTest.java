9cb60a5cb3528838e021e1fc5781e8a488dc5f6f
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-01-16 21:26:39 -0300

    feat: ajuste de teste em service 2

--- src/test/java/br/dev/dantas/point/service/ProducerServiceTest.java
+++ src/test/java/br/dev/dantas/point/service/ProducerServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -38,11 +40,36 @@ class ProducerServiceTest {
 
     @Test
     @DisplayName("findAll() returns a list with all producers")
+    @Order(1)
     void findAll_ReturnsAllProducers_WhenSuccessful() {
         BDDMockito.when(repository.findByName(null)).thenReturn(this.producers);
 
         var producers = service.listAll(null);
         org.assertj.core.api.Assertions.assertThat(producers).hasSameElementsAs(producers);
+    }
+
+    @Test
+    @DisplayName("findAll() returns a list with found producers when name is not null")
+    @Order(2)
+    void findAll_ReturnsFoundProducers_WhenNamePassedAndFound() {
+        var name = "universal";
+        List<Producer> producersFound = this.producers.stream().filter(producer -> producer.getName().equals(name)).toList();
+
+        BDDMockito.when(repository.findByName(name)).thenReturn(producersFound);
+
+        var producers = service.listAll(name);
+        org.assertj.core.api.Assertions.assertThat(producers).hasSize(1).contains(producersFound.get(0));
+    }
+
+    @Test
+    @DisplayName("findAll() returns an empty list when no producer is found by name")
+    @Order(3)
+    void findAll_ReturnsEmptyList_WhenNoNameIsFound() {
+        var name = "x";
+        BDDMockito.when(repository.findByName(name)).thenReturn(Collections.emptyList());
+
+        var producers = service.listAll(name);
+        org.assertj.core.api.Assertions.assertThat(producers).isNotNull().isEmpty();
     }
 
 }
