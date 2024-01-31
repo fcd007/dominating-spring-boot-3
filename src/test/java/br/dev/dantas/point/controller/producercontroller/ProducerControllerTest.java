46d6b648ad048f28eb6d946166da9020bafca652
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-01-30 22:15:21 -0300

    test: producer teste controller

---
+++ src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
@@ -0,0 +1,29 @@
+package br.dev.dantas.point.controller.producercontroller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(ProducerController.class)
+class ProducerControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("findAll() returns a list with all producers")
+    @Order(1)
+    void findAll_ReturnsAllProducers_WhenSuccessful() throws Exception {
+            mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+    }
+}
