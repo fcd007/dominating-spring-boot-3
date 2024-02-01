634f792c4ac66863e55d531fd18f1dfa292f6751
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-01 20:34:24 -0300

    test: producer teste controller

--- src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
+++ src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
@@ -1,14 +1,25 @@
 package br.dev.dantas.point.controller.producercontroller;
 
+import br.dev.dantas.point.domain.Producer;
+import br.dev.dantas.point.repository.ProducerData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(ProducerController.class)
 class ProducerControllerTest {
@@ -17,13 +28,38 @@ class ProducerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private List<Producer> producers;
+
+    @MockBean
+    private ProducerData producerData;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @BeforeEach
+    void init() {
+        var universal = Producer.builder().id(1L).name("universal").createdAt(LocalDateTime.now()).build();
+        var luca = Producer.builder().id(2L).name("Luca").createdAt(LocalDateTime.now()).build();
+        var marvel = Producer.builder().id(3L).name("marvel").createdAt(LocalDateTime.now()).build();
+
+        producers = new ArrayList<>(List.of(universal, luca, marvel));
+
+        BDDMockito.when(producerData.getProducers()).thenReturn(producers);
+    }
+
     @Test
     @DisplayName("findAll() returns a list with all producers")
     @Order(1)
     void findAll_ReturnsAllProducers_WhenSuccessful() throws Exception {
+        var response = readResourceFile("get-producer-null-name-200.json");
             mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT))
                     .andDo(MockMvcResultHandlers.print())
                     .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                    .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws Exception {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }
