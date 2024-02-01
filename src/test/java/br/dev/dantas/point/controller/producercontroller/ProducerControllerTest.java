b6972dc32aac08602462e8433ce61e282b89a516
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-01 20:51:48 -0300

    test: producer teste controller

--- src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
+++ src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
@@ -2,6 +2,7 @@ package br.dev.dantas.point.controller.producercontroller;
 
 import br.dev.dantas.point.domain.Producer;
 import br.dev.dantas.point.repository.ProducerData;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @WebMvcTest(ProducerController.class)
@@ -38,9 +40,9 @@ class ProducerControllerTest {
 
     @BeforeEach
     void init() {
-        var universal = Producer.builder().id(1L).name("universal").createdAt(LocalDateTime.now()).build();
+        var universal = Producer.builder().id(1L).name("marvel").createdAt(LocalDateTime.now()).build();
         var luca = Producer.builder().id(2L).name("Luca").createdAt(LocalDateTime.now()).build();
-        var marvel = Producer.builder().id(3L).name("marvel").createdAt(LocalDateTime.now()).build();
+        var marvel = Producer.builder().id(3L).name("universal").createdAt(LocalDateTime.now()).build();
 
         producers = new ArrayList<>(List.of(universal, luca, marvel));
 
@@ -56,6 +58,32 @@ class ProducerControllerTest {
                     .andDo(MockMvcResultHandlers.print())
                     .andExpect(MockMvcResultMatchers.status().isOk())
                     .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("findAll() returns a list with found producers when name is not null")
+    @Order(2)
+    void findAll_ReturnsFoundProducers_WhenNamePassedAndFound() throws Exception {
+        var name = "marvel";
+
+        var response = readResourceFile("get-producer-marvel-name-200.json");
+        mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT).param("name", name))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("findBAll() returns an empty list when no producer is found by name")
+    @Order(3)
+    void findByAll_ReturnsEmptyList_WhenNoNameIsFound() throws Exception {
+        var name = "x";
+
+        var response = readResourceFile("get-producer-is-found-name-200.json");
+        mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT).param("name", name))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
     }
 
     private String readResourceFile(String fileName) throws Exception {
