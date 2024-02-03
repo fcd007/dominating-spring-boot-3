a86dce17c918e4f80e374b3f302468ad6741e2c3
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-03 18:10:00 -0300

    test: producer teste controller anime get

--- src/main/java/br/dev/dantas/point/controller/animecontroller/IAnimeController.java
+++ src/main/java/br/dev/dantas/point/controller/animecontroller/IAnimeController.java
@@ -1,6 +1,6 @@
 package br.dev.dantas.point.controller.animecontroller;
 
 public interface IAnimeController {
-    public static final String V1_PATH_DEFAULT = "api/v1/animes";
+    public static final String V1_PATH_DEFAULT = "/api/v1/animes";
     public static final String V1_PATH_OTHER = "api/v1/animes/";
 }
