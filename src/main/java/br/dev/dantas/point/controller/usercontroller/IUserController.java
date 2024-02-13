ec89321f97abfaec4bbd9e1e47456673a799de22
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-13 20:58:04 -0300

    refact: ajuste de recursos e pastas

---
+++ src/main/java/br/dev/dantas/point/controller/usercontroller/IUserController.java
@@ -0,0 +1,4 @@
+package br.dev.dantas.point.controller.usercontroller;
+
+public interface IUserController {
+}
--- src/main/java/br/dev/dantas/point/repository/config/BeanConfig.java
+++ src/main/java/br/dev/dantas/point/repository/config/BeanConfig.java
@@ -1,4 +1,4 @@
-package br.dev.dantas.point.config;
+package br.dev.dantas.point.repository.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
