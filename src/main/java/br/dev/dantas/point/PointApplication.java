38d4f62a5be42ed226fffd5569a2633e3c3b5b83
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-23 09:19:05 -0300

    feat: ajuste e add recurso find all

--- src/main/java/br/dev/dantas/point/PointApplication.java
+++ src/main/java/br/dev/dantas/point/PointApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"test.outside", "br.dev.dantas.point"})
+@ComponentScan(basePackages = {"br.dev.dantas"})
 public class PointApplication {
 
     public static void main(String[] args) {
--- src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
+++ src/test/java/br/dev/dantas/point/controller/producercontroller/ProducerControllerTest.java
@@ -1,5 +1,6 @@
 package br.dev.dantas.point.controller.producercontroller;
 
+import br.dev.dantas.point.commons.FileUtils;
 import br.dev.dantas.point.commons.ProducerUtils;
 import br.dev.dantas.point.repository.ProducerData;
 import br.dev.dantas.point.repository.ProducerHardCodeRepository;
@@ -30,6 +31,9 @@ class ProducerControllerTest {
     @MockBean
     private ProducerData producerData;
 
+    @Autowired
+    private FileUtils fileUtils;
+
     @SpyBean
     private ProducerHardCodeRepository repository;
 
@@ -44,16 +48,11 @@ class ProducerControllerTest {
         BDDMockito.when(producerData.getProducers()).thenReturn(producerUtils.newProducerList());
     }
 
-    private String readResourceFile(String fileName) throws Exception {
-        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
-        return new String(Files.readAllBytes(file.toPath()));
-    }
-
     @Test
     @DisplayName("findAll() returns a list with all producers")
     @Order(1)
     void findAll_ReturnsAllProducers_WhenSuccessful() throws Exception {
-        var response = readResourceFile("producer/get-producer-null-name-200.json");
+        var response = fileUtils.readResourceFile("producer/get-producer-null-name-200.json");
         mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -66,7 +65,7 @@ class ProducerControllerTest {
     void findAll_ReturnsFoundProducers_WhenNamePassedAndFound() throws Exception {
         var name = "Marvel";
 
-        var response = readResourceFile("producer/get-producer-marvel-name-200.json");
+        var response = fileUtils.readResourceFile("producer/get-producer-marvel-name-200.json");
         mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT).param("name", name))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -79,7 +78,7 @@ class ProducerControllerTest {
     void findByAll_ReturnsEmptyList_WhenNoNameIsFound() throws Exception {
         var name = "x";
 
-        var response = readResourceFile("producer/get-producer-is-found-name-200.json");
+        var response = fileUtils.readResourceFile("producer/get-producer-is-found-name-200.json");
         mockMvc.perform(MockMvcRequestBuilders.get(IProducerController.V1_PATH_DEFAULT).param("name", name))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -90,8 +89,8 @@ class ProducerControllerTest {
     @DisplayName("save() creates a producer")
     @Order(4)
     void save_CreateProducer_WhenSuccessful() throws Exception {
-        var request = readResourceFile("producer/post-request-producer-200.json");
-        var response = readResourceFile("producer/post-response-producer-201.json");
+        var request = fileUtils.readResourceFile("producer/post-request-producer-200.json");
+        var response = fileUtils.readResourceFile("producer/post-response-producer-201.json");
         var producerToBeSaved = producerUtils.newProducerToSave();
 
         BDDMockito.when(repository.save(ArgumentMatchers.any())).thenReturn(producerToBeSaved);
@@ -110,7 +109,7 @@ class ProducerControllerTest {
     @DisplayName("update() updates a producer")
     @Order(5)
     void update_UpdateProducer_WhenSuccessFul() throws Exception {
-        var request = readResourceFile("producer/put-request-producer-204.json");
+        var request = fileUtils.readResourceFile("producer/put-request-producer-204.json");
 
         mockMvc.perform(MockMvcRequestBuilders
                         .put(IProducerController.V1_PATH_DEFAULT)
@@ -124,7 +123,7 @@ class ProducerControllerTest {
     @DisplayName("update() updates a throw ResponseStatusException not found")
     @Order(6)
     void update_ThrowResponseStatusException_WhenNoProducerIsFound() throws Exception {
-        var request = readResourceFile("producer/put-request-producer-404.json");
+        var request = fileUtils.readResourceFile("producer/put-request-producer-404.json");
 
         mockMvc.perform(MockMvcRequestBuilders
                         .put(IProducerController.V1_PATH_DEFAULT)
