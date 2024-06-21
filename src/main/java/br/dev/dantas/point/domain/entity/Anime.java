c457b422b31e8de394b8c2d56d4850a74843b58f
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-06-21 17:02:22 -0300

    feat(add repository configuration project): project repository

--- src/main/java/br/dev/dantas/point/domain/entity/Anime.java
+++ src/main/java/br/dev/dantas/point/domain/entity/Anime.java
@@ -1,24 +1,36 @@
 package br.dev.dantas.point.domain.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.*;
 
 @Getter
 @Setter
 @Builder
 @ToString
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@With
 public class Anime {
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @CreationTimestamp
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    private static List<Anime> animes = new ArrayList<>();
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime lastUpdatedOn;
 }
--- src/main/java/br/dev/dantas/point/domain/entity/Producer.java
+++ src/main/java/br/dev/dantas/point/domain/entity/Producer.java
@@ -1,23 +1,35 @@
 package br.dev.dantas.point.domain.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.*;
 
 @Getter
 @Setter
 @Builder
 @ToString
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+@With
 public class Producer {
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @CreationTimestamp
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    private static List<Producer> producers = new ArrayList<>();
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime lastUpdatedOn;
 }
