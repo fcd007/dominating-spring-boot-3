69e9487f54cc042a444828c66c1f4fe5938d3238
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-06-27 16:42:31 -0300

    feat(ajuste de componentscan controller): ajuste componente

--- src/main/java/br/dev/dantas/point/AnimeServiceApplication.java
+++ src/main/java/br/dev/dantas/point/AnimeServiceApplication.java
@@ -2,10 +2,8 @@ package br.dev.dantas.point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"br.dev.dantas"})
 public class AnimeServiceApplication {
 
     public static void main(String[] args) {
