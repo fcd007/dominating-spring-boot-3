3c4f9438da94da409e364b5c784819dcf3ebc793
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-06-27 07:02:30 -0300

    refact(ajuste de imports): ajuste de imports

--- src/main/java/br/dev/dantas/point/domain/mappers/AnimeMapper.java
+++ src/main/java/br/dev/dantas/point/domain/mappers/AnimeMapper.java
@@ -6,27 +6,23 @@ import br.dev.dantas.point.controller.animecontroller.response.AnimeGetResponse;
 import br.dev.dantas.point.controller.animecontroller.response.AnimePostResponse;
 import br.dev.dantas.point.controller.animecontroller.response.AnimePutResponse;
 import br.dev.dantas.point.domain.entity.Anime;
+import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AnimeMapper {
 
-    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+  Anime toAnime(AnimePostRequest request);
 
-    Anime toAnime(AnimePostRequest request);
+  Anime toAnime(AnimePutRequest request);
 
-    Anime toAnime(AnimePutRequest request);
+  AnimePostResponse toAnimePostResponse(Anime anime);
 
-    AnimePostResponse toAnimePostResponse(Anime anime);
+  AnimeGetResponse toAnimeGetResponse(Anime anime);
 
-    AnimeGetResponse toAnimeGetResponse(Anime anime);
+  AnimePutResponse toAnimePutResponse(Anime anime);
 
-    AnimePutResponse toAnimePutResponse(Anime anime);
-
-    List<AnimeGetResponse> toAnimeGetResponseList(List<Anime> animes);
+  List<AnimeGetResponse> toAnimeGetResponseList(List<Anime> animes);
 }
--- src/main/java/br/dev/dantas/point/domain/mappers/ProducerMapper.java
+++ src/main/java/br/dev/dantas/point/domain/mappers/ProducerMapper.java
@@ -6,30 +6,26 @@ import br.dev.dantas.point.controller.producercontroller.response.ProducerGetRes
 import br.dev.dantas.point.controller.producercontroller.response.ProducerPostResponse;
 import br.dev.dantas.point.controller.producercontroller.response.ProducerPutResponse;
 import br.dev.dantas.point.domain.entity.Producer;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
-
 import java.util.List;
 import java.util.Optional;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProducerMapper {
 
-    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+  Producer toProducer(ProducerPostRequest request);
 
-    Producer toProducer(ProducerPostRequest request);
+  Producer toProducer(ProducerPutRequest request);
 
-    Producer toProducer(ProducerPutRequest request);
+  ProducerPostResponse toProducerPostResponse(Producer producer);
 
-    ProducerPostResponse toProducerPostResponse(Producer producer);
+  ProducerGetResponse toProducerGetResponse(Producer producer);
 
-    ProducerGetResponse toProducerGetResponse(Producer producer);
+  ProducerGetResponse toProducerGetResponse(Optional<Producer> producer);
 
-    ProducerGetResponse toProducerGetResponse(Optional<Producer> producer);
+  ProducerPutResponse toProducerPutResponse(Producer producer);
 
-    ProducerPutResponse toProducerPutResponse(Producer producer);
-
-    List<ProducerGetResponse> toProducerGetResponseList(List<Producer> producers);
+  List<ProducerGetResponse> toProducerGetResponseList(List<Producer> producers);
 }
