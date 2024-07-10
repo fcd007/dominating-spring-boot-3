7c5c7469e676abf576f6aea2ec91f0f8c3706e2d
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-07-09 21:28:49 -0300

    test(ajustes teste containers projeto): ajustes teste containers

--- src/main/java/br/dev/dantas/point/AnimeServiceApplication.java
+++ src/main/java/br/dev/dantas/point/AnimeServiceApplication.java
@@ -2,6 +2,7 @@ package br.dev.dantas.point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AnimeServiceApplication {
