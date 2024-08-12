93c3e3a7845759d5d9527d8c2f9124f12af36429
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-08-12 06:26:38 -0300

    chore(ajuste de paste e configuracao de seguranca): configuracao de seguranca

--- src/main/java/br/dev/dantas/point/config/SecurityConfig.java
+++ src/main/java/br/dev/dantas/point/config/SecurityConfig.java
@@ -2,30 +2,39 @@ package br.dev.dantas.point.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "/v3/api-docs/**","/swagger-ui/**"};
+  private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "/v3/api-docs/**", "/swagger-ui/**" };
+  private static final String PASSWORD = "1234";
+  private static final String GUEST = "guest";
+  private static final String ADMIN = "admin";
 
-  private static final String ADMIN = "ADMIN";
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    var user = User.withUsername(GUEST).password(passwordEncoder.encode(PASSWORD)).roles(GUEST).build();
+
+    var admin = User.withUsername(ADMIN).password(passwordEncoder.encode(PASSWORD)).roles(ADMIN).build();
+
+    return new InMemoryUserDetailsManager(user, admin);
+  }
 
   @Bean
   SecurityFilterChain securiyFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(
+    return http.authorizeHttpRequests(
             auth -> auth
-                .requestMatchers(WHITE_LIST).permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/animes").hasAuthority(ADMIN)
-                .requestMatchers(HttpMethod.PUT, "/api/v1/producers/").hasAuthority(ADMIN)
+                .requestMatchers(WHITE_LIST)
+                .permitAll()
                 .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .build();
