c81b7152376d26cb9e93faab5109ee2a171c5fdb
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-08-15 21:05:25 -0300

    feat(ajuste de paste e configuracao de seguranca): configuracao de seguranca

--- src/main/java/br/dev/dantas/point/config/SecurityConfig.java
+++ src/main/java/br/dev/dantas/point/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package br.dev.dantas.point.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,27 +17,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-  private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "/v3/api-docs/**", "/swagger-ui/**" };
-  private static final String PASSWORD = "1234";
-  private static final String GUEST = "guest";
-  private static final String ADMIN = "admin";
+  private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "/v3/api-docs/**",
+      "/swagger-ui/**"};
 
   @Bean
   public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-    var user = User.withUsername(GUEST).password(passwordEncoder.encode(PASSWORD)).roles(GUEST).build();
+    var user = User.withUsername("user")
+        .password(passwordEncoder.encode("1234"))
+        .roles("USER")
+        .build();
 
-    var admin = User.withUsername(ADMIN).password(passwordEncoder.encode(PASSWORD)).roles(ADMIN).build();
+    var admin = User.withUsername("admin")
+        .password(passwordEncoder.encode("1234"))
+        .roles("ADMIN")
+        .build();
 
     return new InMemoryUserDetailsManager(user, admin);
   }
 
   @Bean
   SecurityFilterChain securiyFilterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(
-            auth -> auth
-                .requestMatchers(WHITE_LIST)
-                .permitAll()
-                .anyRequest().authenticated())
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(WHITE_LIST).permitAll()
+            .requestMatchers("/api/v1/animes/**").hasRole("USER")
+            .requestMatchers("/api/v1/producers/**").hasRole("USER")
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+            .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .build();
   }
